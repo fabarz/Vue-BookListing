5997a7d84efa2cfce9557763c981cf477fd287e1
Fatah Barzangy <fatahbarzangy@hotmail.com>
2020-07-29 06:47:49 +0100

    Creating the book list component.

---
+++ src/components/BookList.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Book list",
+  data: function() {
+    return {
+      title: "All Books"
+    }
+  }
+}
+</script>
+
+<style>
+  h1, h2 {
+    font-weight: normal
+  }
+</style>
