967c47a06c95bab77130837118e692eb5a46affb
Fatah Barzangy <fatahbarzangy@hotmail.com>
2020-07-29 07:05:39 +0100

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}} : {{book.author}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -7,7 +12,12 @@ export default {
   name: "Book list",
   data: function() {
     return {
-      title: "All Books"
+      title: "All Books",
+      books: [
+        {title: "Self-Reliance", author: "Ralph Waldo Emerson"},
+        {title: "American Gods", author: "Neil Gaiman"},
+        {title: "Amusing Ourselves to Death", author: "Neil Postman"},
+      ]
     }
   }
 }
@@ -16,5 +26,10 @@ export default {
 <style>
   h1, h2 {
     font-weight: normal
+  }
+  ul {
+    list-style-type: none;
+    display: block;
+    margin: 0 10px;
   }
 </style>
